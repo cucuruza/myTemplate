87c9e0a4d497da90d0948dd8bce6020f983b401e
cucuruza <40572743+cucuruza@users.noreply.github.com>
2023-12-07 13:09:40 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -10,46 +10,131 @@ const sync = require('browser-sync').create()
 const jsmin = require('gulp-uglify-es').default
 const compress_images = require('compress-images')
 
-
+// html
 function html() {
 	return src('src/**.html')
 		.pipe(include({
 			prefix: '@@'
 		}))
-		.pipe(htmlmin({
-			collapseWhitespace: true
-		}))
 		.pipe(dest('dist'))
 }
 
+function htmlMin() {
+	return src('dist/**.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(dest('dist-min'))
+}
+
+
+// scss css
 function scss() {
 	return src('src/scss/**.scss')
 	.pipe(sass())
 	.pipe(autoprefixer())
+	.pipe(dest('dist/css'))
+}
+
+function cssMin() {
+	return src('dist/css/**.css')
 	.pipe(csso())
-	.pipe(concat('main.css'))
-	.pipe(dest('dist'))
+	.pipe(dest('dist-min/css'))
 }
 
-function clear() {
-	return del('dist')
-}
 
+// js
 function scripts() {
 	return src([
-		'src/js/example.js',
 		'src/js/main.js'
 	])
-	.pipe(concat('main.min.js'))
-	.pipe(jsmin())
-	.pipe(dest('dist'))
+	.pipe(dest('dist/js'))
 }
 
-//Compress images
+function scriptsMin() {
+	return src([
+		'dist/js/main.js'
+	])
+	.pipe(jsmin())
+	.pipe(dest('dist-min/js'))
+}
+
+
+// fonts
+function fonts() {
+	return src([
+		'scr/fonts/*'
+	])
+	.pipe(dest('dist/fonts'))
+}
+
+function fontsMin() {
+	return src([
+		'dist/fonts/*'
+	])
+	.pipe(dest('dist-min/fonts'))
+}
+
+
+// plugins
+function normalizecss() {
+	return src([
+		'node_modules/normalize.css/normalize.css'
+	])
+	.pipe(dest('dist/plugins/normalize.css/'))
+}
+
+function normalizecssMin() {
+	return src([
+		'node_modules/normalize.css/normalize.css'
+	])
+	.pipe(csso())
+	.pipe(dest('dist-min/plugins/normalize.css/'))
+}
+
+function swiper() {
+	return src([
+		'node_modules/swiper/swiper-bundle.min.css',
+		'node_modules/swiper/swiper-bundle.min.js'
+	])
+	.pipe(dest('dist/plugins/swiper'))
+}
+
+function swiperMin() {
+	return src([
+		'node_modules/swiper/swiper-bundle.min.css',
+		'node_modules/swiper/swiper-bundle.min.js'
+	])
+	.pipe(dest('dist-min/plugins/swiper'))
+}
+
+function bootstrap() {
+	return src([
+		'src/plugins/bootstrap/bootstrap-grid.scss',
+	])
+	.pipe(sass())
+	.pipe(dest('dist/plugins/bootstrap/'))
+}
+
+function bootstrapMin() {
+	return src('dist/plugins/bootstrap/**.css')
+	.pipe(csso())
+	.pipe(dest('dist-min/plugins/bootstrap/'))
+}
+
+
+// images
 function images() {
+  return src([
+		'src/img/*'
+	])
+	.pipe(dest('dist/img'))
+}
+
+function imagesMin() {
   compress_images(
-    "src/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}",
-    "dist/img/",
+    "dist/img/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}",
+    "dist-min/img/",
 		{ compress_force: false, statistic: true, autoupdate: true }, false, 
 		{ jpg: { engine: "mozjpeg", command: ["-quality", "75"] } }, 
 		{ png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
@@ -63,6 +148,20 @@ function images() {
   );
 }
 
+// favicon
+//
+
+
+// clear
+function clear() {
+	return del('dist')
+}
+function clearMin() {
+	return del('dist-min')
+}
+
+
+
 function serve() {
 	sync.init({
 		server: {baseDir: './dist'}
@@ -73,7 +172,8 @@ function serve() {
 	watch('src/js/**.js', series(scripts)).on('change', sync.reload)
 }
 
-exports.build = series(clear, scss, html, scripts)
-exports.serve = series(clear, scss, html, scripts, serve)
-exports.clear = clear
-exports.images = images
+exports.buildmin = series(fontsMin, normalizecssMin, swiperMin, bootstrapMin, htmlMin, cssMin, scriptsMin, imagesMin)
+exports.build = series(clear, fonts, normalizecss, swiper, bootstrap, scss, html, scripts, images)
+exports.dev = series(clear, fonts, normalizecss, swiper, bootstrap, scss, html, scripts, images, serve)
+exports.clear = series(clear, clearMin)
+// exports.images = images
